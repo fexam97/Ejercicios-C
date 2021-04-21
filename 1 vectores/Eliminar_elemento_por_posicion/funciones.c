8c51ad9d30764b9404a3703489e4d43c1b2ee698
Fernando Meza <70779182+fexam97@users.noreply.github.com>
2021-04-21 13:35:21 -0300

    Update funciones.c

--- 1 vectores/Eliminar_elemento_por_posicion/funciones.c
+++ 1 vectores/Eliminar_elemento_por_posicion/funciones.c
@@ -3,7 +3,7 @@
 
 int insertarEnOrden(int* vec, int* ce, int cap, int elem)
 {
-    if(*ce == cap) // si lade la cantidad de elementos es igual a la capacidad devuelve cero ( y aca no pasó nada)
+    if(*ce == cap) // si la cantidad de elementos es igual a la capacidad devuelve cero ( y aca no pasÃ³ nada)
         return 0;  // pd: NUNCA VA A PASAR
 
     int posAct = (*ce) - 1; //declaro la posicion actual inicializandolo en la cantidad de elementos - 1
@@ -11,14 +11,14 @@ int insertarEnOrden(int* vec, int* ce, int cap, int elem)
     while(posAct >= 0 && elem < *(vec + posAct)) //si la posicion actual es mayor o igual a cero
                                                 // && el elemento random es menor que vec[posAct] ; si todo esto se cumple entonces entra
     {
-        *(vec + (posAct + 1)) = *(vec + posAct);// como el elemento random era menor entonces se asignará el valor la posicion actual + 1
+        *(vec + (posAct + 1)) = *(vec + posAct);// como el elemento random era menor entonces se asignarÃ¡ el valor la posicion actual + 1
                                                 // al valor de la posicion del vector actual
 
         posAct--;   //baja una posicion (imaginate, arranca desde 15)
     }
                                         // una vez que la condicion sea falsa, saldra del while
 
-    *(vec + (posAct + 1)) = elem; // la primera posicion guardara el numero mas pequeño ; ADEMAS GRACIAS A ESTO SE ESTA CARGANDO EL VECTOR
+    *(vec + (posAct + 1)) = elem; // la primera posicion guardara el numero mas pequeÃ±o ; ADEMAS GRACIAS A ESTO SE ESTA CARGANDO EL VECTOR
 
     (*ce)++; // aumenta la cantidad de elementos
 
