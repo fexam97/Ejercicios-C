d7beec83e50aa42b1bad418b0e2fb5c2009b55ed
Fernando Meza <70779182+fexam97@users.noreply.github.com>
2021-04-29 08:22:12 -0300

    Update main.c

--- 3 matrices/matriz_identidad/main.c
+++ 3 matrices/matriz_identidad/main.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define FIL 4
 #define COL 4
 
-void mostrarMatrizIdentidad(int mat[][COL], int fil, int col);
-int esMatrizIdentidad(int mat[][COL], int fil, int col);
+void mostrarMatriz(int mat[][COL], int fil, int col);
+bool esMatrizIdentidad(int mat[][COL], int fil, int col);
 
 
 
 
 int main()
 {
-    int mat[FIL][COL];
+    int mat[FIL][COL]={{1, 0, 0, 0},{0, 1, 0, 0},{0, 0, 1, 0}, {0, 0, 0, 1}};
 
-    mostrarMatrizIdentidad(mat, FIL, COL);
+    mostrarMatriz(mat, FIL, COL);
 
-    int dato = esMatrizIdentidad(mat, FIL, COL);
-
-    dato? printf("\n Es de identidad") : printf("\n No es de identidad");
+    esMatrizIdentidad(mat, FIL, COL)? printf("\n Es de identidad") : printf("\n No es de identidad");
 
     return 0;
 }
 
-void mostrarMatrizIdentidad(int mat[][COL], int fil, int col)
+void mostrarMatriz(int mat[][COL], int fil, int col)
 {
+    (fil==col)? printf("\n es matriz cuadrada \n") : printf("\n No es una matriz cuadrada \n");
+
     printf("\n Matriz de %d x %d :  \n\n", fil , col);
 
     for(int i = 0; i < fil ; i++)
     {
         for(int j = 0; j < col ; j++)
         {
-            (i == j)? (mat[i][j] = 1) : (mat[i][j] = 0);
+            //(i == j)? (mat[i][j] = 1) : (mat[i][j] = 0);
             printf("%d \t", mat[i][j]);
         }
         printf("\n");
@@ -39,13 +40,13 @@ void mostrarMatrizIdentidad(int mat[][COL], int fil, int col)
 
 }
 
-int esMatrizIdentidad(int mat[][COL], int fil, int col)
+bool esMatrizIdentidad(int mat[][COL], int fil, int col)
 {
     for(int i = 0; i < fil ; i++)
     {
         for(int j = 0; j < col ; j++)
         {
-            if( i==j || mat[i][j]==0 )  // va a compilar pero no retorna nada
+            if( i==j || mat[i][j]==0 )  // hasta llegue pero no va a compilar
                 return 0;
         }
     }
